dc7242d0d36978a6dffcef8c9d10d85f2cabcda7
Maximilian Harr <maximilian.harr@gmail.com>
2020-09-05 15:56:14 +0200

    Removed init.sh for latex docker

--- latex/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
-# Greetings
-printf "${NC}--------------------------------------------------${NC}\n"
-printf "${GREEN}Hello $(whoami)! Welcome to $(hostname -s)!${NC}\n"
-printf "${GREEN}You are running ROS ${ROS_VERSION} ${ROS_DISTRO} !${NC}\n"
-printf "${NC}--------------------------------------------------${NC}\n"
